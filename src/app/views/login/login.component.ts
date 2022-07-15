5b248c74edbb56bb0600fe727e975a86953920fd
Franciel Cantanhede <franciel_cr@hotmail.com>
2022-07-15 12:16:36 -0300

    blacklist

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -8,6 +8,7 @@ import { LoginService } from 'src/app/services/login.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginModel: any;
 
   constructor(private loginService: LoginService) { }
 
@@ -15,15 +16,26 @@ export class LoginComponent implements OnInit {
   }
 
   userModel = new User()
-  mensagem = ""
-
-  receberDados() {
+  mensagem = "";
+  receberDados(): void {
     console.log(this.userModel)
-    this.loginService.login(this.userModel).subscribe((response) => {
-      console.log("response:", response)
-      console.log("O status Code é:", response.status)
-      console.log("O Token de permissão é:", response.body.accessToken)
 
+    const listaPalavras: string[] = ["select ", "from ", "drop ", "or ", "having ", "group ", "by ", "insert ", "exec ", "\"", "\'", "-- ", " #", "*", ";" ]
+
+    listaPalavras.forEach(palavra => {
+      if (this.userModel.email?.toLowerCase().includes(palavra)) {
+        this.mensagem = "Dados Inválidos"
+        return;
+        
+      }
+      
+    });
+
+    this.loginService.login(this.userModel).subscribe((response) => {
+      
+      /*console.log("response:", response)
+      console.log("O status Code é:", response.status)
+      console.log("O Token de permissão é:", response.body.accessToken)*/
       this.mensagem = "Bem vindo " + response.body.user.nome
       console.log(this.mensagem)
     }, (responseErro) => {
